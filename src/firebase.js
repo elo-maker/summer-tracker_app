90d708bef487a1bf71ffb8b83f0dc15840c4c5f6
elo-maker <emilylopick@gmail.com>
2026-05-27 21:14:07 +0800

    add firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -15,12 +15,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_AUTH_DOMAIN_HERE",
-  projectId: "PASTE_YOUR_PROJECT_ID_HERE",
-  storageBucket: "PASTE_YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "PASTE_YOUR_APP_ID_HERE",
+  apiKey: "AIzaSyA8v1rcuf_uNh4viioK4zz_0Dto6C-P9n8",
+  authDomain: "summer-tracker-17073.firebaseapp.com",
+  projectId: "summer-tracker-17073",
+  storageBucket: "summer-tracker-17073.firebasestorage.app",
+  messagingSenderId: "395262175183",
+  appId: "1:395262175183:web:c1f338ad63481d6f7594ba"
 };
 
 const app = initializeApp(firebaseConfig);
